b3765bc510b4bf5697d4c1e41306b231c97c876d
root <mngenge@gmail.com>
2023-04-25 13:42:20 -0700

    correction the code output for redirection command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,15 +2,17 @@
 package ( 'nginx':
   ensure => installed,
 }
+
 file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
+  after  => 'server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+  path    => '/var/www/html/index.html'
 }
 
 service { 'nginx':
